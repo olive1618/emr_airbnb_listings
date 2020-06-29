2d072ce570c2b6378e81c8729eb8b9e7bdc44685
Emily Shouppe <emily.shouppe@storicard.com>
2020-06-28 21:58:46 -0500

    Change up iteration approach

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # emr_airbnb_listings
+
+
+## Analysis
+
+1. Predict reviews or per night price by other factors
+2. Per night price by home type and neighborhood
+3. Price distribution by city and by count in city
--- main.py
+++ main.py
@@ -10,27 +10,41 @@ SQL_CONTEXT = SQLContext(SPARK_CONTEXT)
 
 
 def load_and_concatenate_files():
+    """Load the file for the first city to a pyspark dataframe. Then iterate through the list of
+    file names, load each to a dataframe, and merge each with the main DF
+    """
     data_schema = get_schema()
     data_dir = Path('data')
-    all_dfs = {}
+    file_names = [file_loc for file_loc in data_dir.iterdir()]
 
-    for file_loc in data_dir.iterdir():
+    first_file = file_names.pop()
+    main_df = (
+        SQL_CONTEXT
+        .read.format('com.databricks.spark.csv')
+        .options(header='true', schema=data_schema, inferSchema=False, delimiter=',')
+        .load(str(first_file))
+    )
+    city_name = '_'.join(first_file.stem.split('_')[:-1])
+    main_df = main_df.withColumn('city_name', F.lit(city_name))
+
+    for file_loc in file_names:
         city_name = '_'.join(file_loc.stem.split('_')[:-1])
         df = (
             SQL_CONTEXT
             .read.format('com.databricks.spark.csv')
             .options(header='true', schema=data_schema, inferSchema=False, delimiter=',')
-            .load(file_loc)
+            .load(str(file_loc))
         )
-        all_dfs[city_name] = df
-    
-    return all_dfs
+        df = df.withColumn('city_name', F.lit(city_name))
+        main_df = main_df.union(df)
+
+    return main_df
 
 
 def get_schema():
     """Create a pyspark schema for the listing data files. Every file has the same column names and
-    types. The variable col_items is a dictionary where each key is a column name and its item is the
-    column data type. 
+    types. The variable col_items is a dictionary where each key is a column name and its item is
+    the column data type
     """
     col_items = {
         'id': 'int', 'name': 'str', 'host_id': 'int', 'host_name': 'str',
@@ -44,7 +58,7 @@ def get_schema():
     }
     listing_schema = []
 
-    for col_name, col_type in col_info.items():
+    for col_name, col_type in col_items.items():
         listing_schema.append(
             T.StructField(
                 name=col_name,
@@ -52,7 +66,11 @@ def get_schema():
                 nullable=True
             )
         )
- 
+
     listing_schema = T.StructType(listing_schema)
 
     return listing_schema
+
+
+if __name__ == "__main__":
+    data_dfs = load_and_concatenate_files()
